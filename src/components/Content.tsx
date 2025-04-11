32ba6e7adf3273c30924b69ab99ba135bb111188
Mohammed-Faisal-L <mdfaisal.a334@gmail.com>
2025-04-11 18:19:00 +0530

    implemented hero section

---
+++ src/components/Content.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import articleImage from '../assets/articleImage.webp';
+
+const MyComponent: React.FC = () => {
+    return (
+        <div className="flex flex-col-reverse md:flex-row w-full">
+            <div className="w-full md:w-1/2 our-story-section">
+                <div className="text-left">
+                    <h1 className="our-story-heading">Our Story</h1>
+                    <p className="our-story-paragraph">
+                        Launced in 2015, Exclusive is South Asia's premier online shopping marketplace with an active presence in India.
+                        Supported with a wide range of tailored marketing, data and service solutions, Exclusive has 10,500 sailers and 300 brands
+                        and serves 3 million customers across the region.
+                    </p>
+                    <p className="our-story-paragraph">
+                        Exclusive has more than 1 Million products to offer, growing fast. Exclusive offers a diverse assortment in categories
+                        ranging from customers.
+                    </p>
+                </div>
+            </div>
+
+            <div className="w-full md:w-1/2 p-4 md:p-6 md:h-[500px] flex items-center justify-center">
+                <img
+                    src={articleImage}
+                    alt="Our Story"
+                    className="h-full md:h-[500px] lg:h-auto w-auto max-w-full rounded object-contain"
+                />
+            </div>
+        </div>
+    );
+};
+
+export default MyComponent;
