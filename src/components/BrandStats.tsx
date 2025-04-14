6ec938db0ea422f839d0b95419fa3958ca151fd8
yadavittal <vittalyadav8328@gmail.com>
2025-04-14 11:24:59 +0530

    feat: added service card

--- src/components/BrandStats.tsx
+++ src/components/BrandStats.tsx
@@ -2,7 +2,7 @@ import Cards from "./Cards";
 
 function BrandStats() {
   return (
-    <div id="about" className="w-[100%] flex flex-col bg-gray-700 items-center justify-center">
+    <div className="w-full flex flex-col items-center justify-center">
       <Cards />
     </div>
   );
